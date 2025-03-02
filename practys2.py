86d0cc23f4cc048e8ee2994888daf9b614456020
Yazan Hamoud <hamoudyazan@gmail.com>
2025-03-03 00:49:50 +0100

    74 (07:10:24) listbox 📋

--- practys2.py
+++ practys2.py
@@ -2589,6 +2589,72 @@ from multiprocessing import Process, cpu_count
 
 
 
+#74 (07:10:24​) listbox 📋
+
+# listbox = a listbox of selectable text items within it's own container
+
+#from tkinter import *
+
+
+#def sumbit():
+    #food = []
+    #for i in listbox.curselection():
+        #food.insert(i, listbox.get(i))
+    #print(f"You have ordered {listbox.get(listbox.curselection())}")
+    #print("You have ordered")
+    #for i in food:
+        #print(i)
+        
+
+#def add():
+    #listbox.insert(listbox.size(),entry_box.get())
+    #listbox.config(height=listbox.size())
+    
+#def delete():
+    #for i in reversed(listbox.curselection()):
+        #listbox.delete(i)
+
+    
+    #listbox.delete(listbox.curselection())
+    #listbox.config(height=listbox.size())
+    
+    
+    
+#window = Tk()
+
+
+#listbox = Listbox(window,
+                  #background="#A8A632",
+                  #font=("Constantia",35),
+                  #width=15,
+                  #selectmode=MULTIPLE)
+
+
+#listbox.insert(1,"pizza")
+#listbox.insert(2,"pasta")
+#listbox.insert(3,"garlic bread")
+#listbox.insert(4,"soup")
+#listbox.insert(5,"salad cezar")
+#listbox.pack()
+
+#listbox.config(height=listbox.size())
+
+#entry_box = Entry(window)
+#entry_box.pack()
+
+#sumbit_button = Button(window,text="sumbit",command=sumbit)
+#sumbit_button.pack()
+
+#add_button = Button(window,text="add",command=add)
+#add_button.pack()
+
+#delete_button = Button(window,text="delete",command=delete)
+#delete_button.pack()
+
+#window.mainloop()
+
+
+
 
 
 
